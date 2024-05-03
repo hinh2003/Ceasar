c79b3c37d8ad46cd11df233f78aadd6380f87791
hinh2003 <vanhinh2003@gmail.com>
2024-05-03 22:28:49 +0700

    done

---
+++ Baomat/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Baomat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.txtDecrypted = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(215, 94);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(475, 73);
+            this.txtInput.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(254, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(226, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(349, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Thuật toán mã hóa Ceasar";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 29);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nhập mã hóa";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 207);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 29);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Nhập Key";
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(215, 202);
+            this.txtKey.Multiline = true;
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(131, 34);
+            this.txtKey.TabIndex = 5;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(215, 263);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(475, 81);
+            this.txtOutput.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 288);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(161, 29);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Chuỗi mã hóa";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(725, 288);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 47);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Mã hóa";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(725, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 47);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Đọc File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(725, 189);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 47);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Lưu file";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(725, 381);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(122, 47);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Giải mã";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // txtDecrypted
+            // 
+            this.txtDecrypted.Location = new System.Drawing.Point(215, 371);
+            this.txtDecrypted.Multiline = true;
+            this.txtDecrypted.Name = "txtDecrypted";
+            this.txtDecrypted.Size = new System.Drawing.Size(475, 81);
+            this.txtDecrypted.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 399);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(160, 29);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Chuỗi giải mã";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(859, 464);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtDecrypted);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Ceasar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox txtDecrypted;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Baomat/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Baomat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Lấy dữ liệu từ TextBox và khóa
+            string plaintext = txtInput.Text;
+            if (string.IsNullOrEmpty(plaintext))
+            {
+                MessageBox.Show("Vui lòng nhập dữ liệu.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (!int.TryParse(txtKey.Text, out int key))
+            {
+                MessageBox.Show("Vui lòng nhập một số nguyên làm khóa.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Gọi hàm mã hóa Caesar và hiển thị kết quả
+            string encryptedText = CaesarEncrypt(plaintext, key);
+            txtOutput.Text = encryptedText;
+        }
+
+        // Hàm mã hóa Caesar
+        private string CaesarEncrypt(string input, int key)
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (char ch in input)
+            {
+                if (char.IsLetter(ch))
+                {
+                    char encryptedChar = (char)(ch + key);
+                    if (char.IsUpper(ch))
+                    {
+                        if (encryptedChar > 'Z')
+                            encryptedChar = (char)(encryptedChar - 26);
+                    }
+                    else
+                    {
+                        if (encryptedChar > 'z')
+                            encryptedChar = (char)(encryptedChar - 26);
+                    }
+                    result.Append(encryptedChar);
+                }
+                else
+                {
+                    result.Append(ch);
+                }
+            }
+
+            return result.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filename = openFileDialog.FileName;
+                string fileContent = File.ReadAllText(filename);
+                txtInput.Text = fileContent;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string encryptedText = txtOutput.Text;
+            if (string.IsNullOrEmpty(encryptedText))
+            {
+                MessageBox.Show("Không có dữ liệu cần mã hóa.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            {
+
+            }
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text files (*.txt)|*.txt";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filename = saveFileDialog.FileName;
+                // Ghi dữ liệu đã mã hóa vào tệp
+                File.WriteAllText(filename, encryptedText);
+                MessageBox.Show("Dữ liệu đã được lưu vào " + filename, "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Lấy dữ liệu từ TextBox và khóa
+            string encryptedText = txtOutput.Text;
+            if (!int.TryParse(txtKey.Text, out int key))
+            {
+                MessageBox.Show("Vui lòng nhập một số nguyên làm khóa.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Gọi hàm giải mã Caesar và hiển thị kết quả
+            string decryptedText = CaesarDecrypt(encryptedText, key);
+            txtDecrypted.Text = decryptedText;
+        }
+
+        // Hàm giải mã Caesar
+        private string CaesarDecrypt(string input, int key)
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (char ch in input)
+            {
+                if (char.IsLetter(ch))
+                {
+                    char decryptedChar = (char)(ch - key);
+                    if (char.IsUpper(ch))
+                    {
+                        if (decryptedChar < 'A')
+                            decryptedChar = (char)(decryptedChar + 26);
+                    }
+                    else
+                    {
+                        if (decryptedChar < 'a')
+                            decryptedChar = (char)(decryptedChar + 26);
+                    }
+                    result.Append(decryptedChar);
+                }
+                else
+                {
+                    result.Append(ch); // Giữ nguyên các ký tự không phải chữ cái
+                }
+            }
+
+            return result.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
